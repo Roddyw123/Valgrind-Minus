8194e019cced55127a7d4f1c250119ced9c58dcf
Roddyw123 <yuitiwata@gmail.com>
2025-11-05 17:47:55 +0000

    [rw2024] feat: copied test program from spec and added .ignore and c_app_properties

---
+++ src/llvm_pass.cpp
@@ -0,0 +1,8 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+
+using namespace llvm;
+
