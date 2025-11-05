21f3985ea1f19fe81cdaf422f4e3ad69b9cd39a4
Roddyw123 <yuitiwata@gmail.com>
2025-11-05 18:23:59 +0000

    [rw2024] feat: extended malloc_intercept.c by adding a free intercept function for fun. Fixed bugs using Generative AI.

--- src/malloc_intercept.c
+++ src/malloc_intercept.c
@@ -1,17 +1,35 @@
 #include <dlfcn.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <malloc/malloc.h>
 
-void *malloc(size_t size) 
+static size_t allocated_bytes = 0;
+
+void *malloc(size_t size)
     {
         static void *(*og_malloc)(size_t) = NULL;
-        if (!og_malloc) 
+        if (!og_malloc)
             {
                 og_malloc = dlsym (RTLD_NEXT, "malloc");
             }
         void *ptr = og_malloc (size);
-        static size_t allocated_bytes;
-        allocated_bytes += size;
+        __sync_fetch_and_add(&allocated_bytes, size);
         fprintf (stderr, "malloc(%zu) -> total: %zu bytes\n", size, allocated_bytes);
         return ptr;
     }
+
+/* Extension: Trivially add for free */
+
+void free(void *ptr) {
+    static void (*og_free)(void*) = NULL;
+    if (!og_free)
+        {
+            og_free = dlsym (RTLD_NEXT, "free");
+        }
+    if (ptr != NULL)
+        {
+            size_t size = malloc_usable_size(ptr);
+            __sync_fetch_and_sub(&allocated_bytes, size);
+        }
+    og_free (ptr);
+} 
