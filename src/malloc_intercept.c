d949f5bcce2d0ecfe2836e223a0aadca920fe57a
Roddyw123 <yuitiwata@gmail.com>
2025-11-05 16:52:16 +0000

    [rw2024] feat: fix global allocated_bytes to function scope, output bytes to stream stderr and finished the function.

--- src/malloc_intercept.c
+++ src/malloc_intercept.c
@@ -1,14 +1,17 @@
 #include <dlfcn.h>
 #include <stddef.h>
-
-static size_t allocated_bytes;
+#include <stdio.h>
 
 void *malloc(size_t size) 
     {
         static void *(*og_malloc)(size_t) = NULL;
         if (!og_malloc) 
             {
-                og_malloc = dlysm( RTLD_NEXT, "malloc");
+                og_malloc = dlysm (RTLD_NEXT, "malloc");
             }
         void *ptr = og_malloc (size);
+        static size_t allocated_bytes;
+        allocated_bytes += size;
+        fprintf (stderr, "malloc(%zu) -> total: %zu bytes\n", size, allocated_bytes);
+        return ptr;
     }
