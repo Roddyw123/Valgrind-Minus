c5c8b95d073b0647976dc199089dca374f7938dc
Roddyw123 <yuitiwata@gmail.com>
2025-11-05 18:01:45 +0000

    [rw2024] feat: implemented llvm pass via ModulePass in C++

--- src/llvm_pass.cpp
+++ src/llvm_pass.cpp
@@ -6,3 +6,27 @@
 
 using namespace llvm;
 
+namespace {
+  struct CountPass : public ModulePass {
+    static char ID;
+    CountPass() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M) override {
+      unsigned int functions = 0;
+      unsigned int blocks = 0;
+      for (Function &F : M) {
+        if (!F.isDeclaration()) {
+          functions ++;
+          for (BasicBlock &BB : F) {
+            blocks ++;
+          }
+        }
+      }
+      errs() << "The program has " << functions << " and " << blocks << " blocks\n";
+      return false;
+    }
+  };
+}
+
+char CountPass::ID = 0;
+static RegisterPass<CountPass> X("countpass", "Count Functions and Blocks pass");
